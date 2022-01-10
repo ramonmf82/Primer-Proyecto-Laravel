4bce6e95a7542f09408e60dc1d5f73044156127e
ramon <ramonmartinezf@gmail.com>
2022-01-10 12:20:31 +0100

    arreglos en nav y nosotros

--- resources/views/nosotros.blade.php
+++ resources/views/nosotros.blade.php
@@ -4,40 +4,35 @@
 
 @section('content')
 
-    <h1>Acerca de mí</h1>
+    <h1 class="py-5">Acerca de mí</h1>
 
-    <p>Mi nombre es Ramón Martínez Fernández</p>
-    <p>Soy nuevo en este mundo del desarrollo Web.</p>
-    <p>Empecé en Junio del año 2021 con una Bootcamp de la academia Aulab.</p>
+    <p>Mi nombre es <b>Ramón Martínez Fernández</b>. Soy nuevo en este mundo del desarrollo Web.</p>
+    <p>Empecé en Junio del año 2021 con una Bootcamp de la academia <b>AulaB</b>.</p>
     <br>
     <p>No fue mi primera experciencia en el mundo de la programación.</p>
-    <p>Yo estudié Matemáticas en la Universidad</p>
-    <p>y ya conocía los fundamentos básicos de la programación.</p>
-    <p>Los lenguajes que ya conocía eran C y Java.</p>
+    <p>Yo estudié <b>Matemáticas</b> en la Universidad y ya conocía los fundamentos básicos de la programación.</p>
+    <p>Los lenguajes que ya conocía eran <b>C</b> y <b>Java</b>.</p>
     <p>Así que la programación orientada a objetos tampoco era algo nuevo para mí.</p>
     <br>
-    <p>En este curso que hice con Aulab he aprendido:</p>
-    <p>HTML, CSS, JavaScript, Php</p>
-    <p>Y los frameworks Bootstrap y Laravel.</p>
-    <p>Además de haber trabajado con la metodología Agil.</p>
+    <p>En este curso que hice con <b>AulaB</b> he aprendido:</p>
+    <p><b>HTML</b>, <b>CSS</b>, <b>JavaScript</b>, <b>Php</b>, bases de datos relacionales,</p>
+    <p>además de trabajar con los frameworks <b>Bootstrap</b> y <b>Laravel</b>.</p>
+    <p>Aparte de esto, hemos aprendido a trabajar con la metodología <b>Agil</b>.</p>
     <br>
-    <p>A día de hoy me dedico a profundizar más en todos esos lenguajes.</p>
-    <p>Además estoy empezando a estudiar otro framework llamado VueJS.</p>
+    <p>A día de hoy me dedico a profundizar más en todos esos lenguajes y estoy empezando a estudiar <b>VueJS</b>.</p>
     <br>
-    <p>Aparte de esto, llevo ya más 10 años dedicado a la enseñanza.</p>
-    <p>Soy profesor particular de las asignaturas de</p>
-    <p>Matemáticas, física y química</p>
-    <p>A nivel de ESO, Bachillerato y Universitario.</p>
+    <p>Por otra parte, llevo ya más 10 años dedicado a la enseñanza.</p>
+    <p>Soy profesor particular de las asignaturas de Matemáticas, física y química,</p>
+    <p>a nivel de ESO, Bachillerato y Universitario.</p>
     <br>
-    <p>Ahora me gustaría encontrar mi primer trabajo en este nuevo mundo</p>
-    <p>de la programación Web.</p>
-    <p>Como no tengo mucha experiencia, se me ha ocurrido crear este pequeño proyecto con Laravel</p>
-    <p>Para así darme a conocer.</p>
+    <p>Ahora me gustaría encontrar mi primer trabajo en este nuevo mundo de la programación Web.</p>
+    <p>Como no tengo mucha experiencia, se me ha ocurrido crear este pequeño proyecto con Laravel para así darme a conocer.</p>
+    <br>
     <p>Aquí teneis un enlace a mi página personal de Github donde estoy empezando a </p>
     <p>subir todos los pequeños proyectos que voy haciendo desde que terminé la Bootcamp.</p>
-    
+ 
     <a href="https://github.com/ramonmf82" target=”_blank”>Mi github</a>
-
+    <br>
     <p>En este otro enlace, tambien a Github está el proyecto fin de curso en Aulab</p>
     <p>que realizé con mi compañera Jessica.</p>
 
